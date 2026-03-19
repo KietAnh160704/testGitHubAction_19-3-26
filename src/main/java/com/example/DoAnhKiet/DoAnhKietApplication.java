bd1b4bcd6cd79c5c28b7db2a0057b7aa0b3fbcf3
KietAnh1607 <161666529+KietAnh1607@users.noreply.github.com>
2026-03-19 10:35:19 +0700

    first commit

---
+++ src/main/java/com/example/DoAnhKiet/DoAnhKietApplication.java
@@ -0,0 +1,13 @@
+package com.example.DoAnhKiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnhKietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnhKietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DoAnhKiet/DoAnhKietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DoAnhKiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnhKietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
